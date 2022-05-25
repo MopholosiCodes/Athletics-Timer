285205d8b32529b470e89408c756f86bdbb67d27
Mopholosi Monyollo <mopholosiCodes@gmail.com>
2022-05-25 09:20:55 +0200

    init commit

---
+++ BusinessLayer/StopWatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class StopWatch
+    {
+        private DateTime StartTime;
+        private DateTime EndTime;
+        private bool Running = false;
+
+        // Start timer
+        public void Start()
+        {
+            if(Running) 
+                throw new InvalidOperationException("Stopwatch already started");
+
+            StartTime = DateTime.Now;
+            Running = true;
+        }
+
+        // Stop timer
+        public void Stop()
+        {
+            if (!Running) 
+                throw new InvalidOperationException("Stopwatch already stopped");
+
+            EndTime = DateTime.Now;
+            Running = false;
+        }
+
+        // Difference
+        public TimeSpan GetDuration()
+        {
+            return EndTime - StartTime;
+        }
+    }
+}
---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopwatch = new StopWatch();
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.GetDuration().ToString());
+            Console.ReadLine();
+        }
+    }
+}
